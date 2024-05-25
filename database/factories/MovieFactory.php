e0ae8cb2ad54bea4c1f6a5665f625aae4de8e7e2
AtanUncensored <153695653+AtanUncensored@users.noreply.github.com>
2024-05-26 07:16:43 +0800

    main

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -19,11 +19,11 @@ class MovieFactory extends Factory
     public function definition(): array
     {
             return [
-                'title' => fake()->words(3, true),
-    'director' => fake()->words(3, true),
-    'description' => fake()->sentence(),
-    'release_date' => fake()->date(),
-    'genre_id' => Genre::pluck('id')->random(),
-            ];
-    }
+            'title' => fake()->words(3, true),
+            'director' => fake()->words(3, true),
+            'description' => fake()->sentence(),
+            'release_date' => fake()->date(),
+            'genre_id' => Genre::pluck('id')->random(),
+                    ];
+            }
 }
